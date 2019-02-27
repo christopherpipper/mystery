187f9bb392a9204e049c25ee2e82e9c8e3fa8766
christopherpipper <christopherpipper@users.noreply.github.com>
2019-02-27 15:26:15 -0400

    mystery.cpp

--- mystery.cpp
+++ mystery.cpp
@@ -10,7 +10,7 @@ void print(auto A)
    cout<<endl;
 }
 
-void mystery1(auto& Data)
+void mystery1(auto& Data)// also known as bubble sort dtat
 {
   cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
@@ -24,7 +24,37 @@ void mystery1(auto& Data)
   }//end outer for (this brace is needed to include the print statement)
 
 }
+void mystery2(auto& Data)// also given as insertion sort
+{
+	for (int i = 1; i < Data.size( ); i++)
+	{
+		int j = i;
+		
+		while(j>0 and Data[j] < Data[j-1])
+		{
+			swap(Data[j], Data[j-1];
+			j--;
+		}
+	}
+}
 
+void mystery3(auto& Data)
+{
+	for (int i = 0; i < Data.size( ); i++)
+	{
+		int min = i; // index of the smallest element 
+		
+		for(int j = i+1; j < Data.size( ); j++)
+		{
+			if( Data[j] < Data[min])
+			{
+				min = j;
+			}
+		}
+		
+		swap(Data[i], Data[min])
+	}
+}
 //... Other mysteries...
 
 int main()
